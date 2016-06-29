a373cd4284c3758d905d9ae7f0fa665118a45da1
Denis Bakhvalov <denis.bakhvalov@nokia.com>
2016-06-29 10:35:02 +0200

    added error handling

--- SysVSharedMemory/main.cpp
+++ SysVSharedMemory/main.cpp
@@ -3,14 +3,25 @@
 #include <sys/shm.h>
 #include <unistd.h>
 #include <cstring>
+#include <iostream>
 
 int main()
 {
 	key_t key = ftok("/tmp/mem.temp", 1);
 
 	int shmId = shmget(key, 1024*1024*1024, IPC_CREAT);
+	if (shmId == -1)
+	{
+		std::cout << "Shared memory was not created.\n";
+		return 1;
+	}
 
 	void* allocShMem = shmat(shmId, NULL, 0);
+	if (allocShMem == (void *) -1)
+	{
+		std::cout << "Shared memory was not attached.\n";
+		return 1;
+	}
 
 	memset(allocShMem, 42, 1024*1024*1024);
 
