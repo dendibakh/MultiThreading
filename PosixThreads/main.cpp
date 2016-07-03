ad4701dc24937192e1676ee115131664f8c9fe25
Denis Bakhvalov <dendibakh@gmail.com>
2016-07-03 14:54:43 +0200

    added sync examples

--- PosixThreads/main.cpp
+++ PosixThreads/main.cpp
@@ -3,10 +3,39 @@
 #include <fstream>      // std::filebuf
 #include <unistd.h>
 
-void* start_routine(void *arg)
+pthread_mutex_t mp;
+pthread_spinlock_t msp;
+pthread_rwlock_t mrwp;
+
+void* mutex_locker(void *arg)
 {
-	int* intVal = (int*) arg;
-	++(*intVal);
+	pthread_mutex_lock(&mp);
+	pause();
+	pthread_mutex_unlock(&mp);
+	return arg;
+}
+
+void* spinlock_locker(void *arg)
+{
+	pthread_spin_lock(&msp);
+	pause();
+	pthread_spin_unlock(&msp);
+	return arg;
+}
+
+void* wr_locker(void *arg)
+{
+	pthread_rwlock_wrlock(&mrwp);
+	pause();
+	pthread_rwlock_wrlock(&mrwp);
+	return arg;
+}
+
+void* rd_locker(void *arg)
+{
+	pthread_rwlock_rdlock(&mrwp);
+	pause();
+	pthread_rwlock_rdlock(&mrwp);
 	return arg;
 }
 
@@ -21,13 +50,55 @@ int main()
 		os.flush();
 	}
 
-	pthread_t thread1;
+	pthread_mutex_init(&mp, NULL);
+	pthread_spin_init(&msp, PTHREAD_PROCESS_PRIVATE);
+	pthread_rwlock_init(&mrwp, NULL);
+
 	int val = 0;
-	pthread_create(&thread1, NULL, start_routine, &val);
+
+	// mutex task
+	pthread_t thread1;
+	pthread_create(&thread1, NULL, mutex_locker, &val);
+
+	sleep (1);
+
+	// this thread will be blocked
+	pthread_t thread2;
+	pthread_create(&thread2, NULL, mutex_locker, &val);
+
+	// spinlock task
+	pthread_t thread3;
+	pthread_create(&thread3, NULL, spinlock_locker, &val);
+
+	sleep (1);
+
+	// this thread will be blocked
+	pthread_t thread4;
+	pthread_create(&thread4, NULL, spinlock_locker, &val);
+
+	// read-write lock task
+	pthread_t thread5;
+	pthread_create(&thread5, NULL, wr_locker, &val);
+
+	sleep (1);
+
+	// this thread will wait write lock
+	pthread_t thread6;
+	pthread_create(&thread6, NULL, wr_locker, &val);
+
+	// this thread will wait read lock
+	pthread_t thread7;
+	pthread_create(&thread7, NULL, rd_locker, &val);
 
 	pause();
 
 	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	pthread_join(thread3, NULL);
+	pthread_join(thread4, NULL);
+	pthread_join(thread5, NULL);
+	pthread_join(thread6, NULL);
+	pthread_join(thread7, NULL);
 
 	return 0;
 }
