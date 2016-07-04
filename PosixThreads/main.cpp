037fdf1996e69c99ad595a6d6934680f6d34f4a4
Denis Bakhvalov <denis.bakhvalov@nokia.com>
2016-07-04 09:49:29 +0200

    added task for condVars and barriers

--- PosixThreads/main.cpp
+++ PosixThreads/main.cpp
@@ -3,40 +3,150 @@
 #include <fstream>      // std::filebuf
 #include <unistd.h>
 
-pthread_mutex_t mp;
-pthread_spinlock_t msp;
-pthread_rwlock_t mrwp;
-
-void* mutex_locker(void *arg)
+namespace Locks
 {
-	pthread_mutex_lock(&mp);
-	pause();
-	pthread_mutex_unlock(&mp);
-	return arg;
+	pthread_mutex_t mp;
+	pthread_spinlock_t msp;
+	pthread_rwlock_t mrwp;
+
+	void* mutex_locker(void *arg)
+	{
+		pthread_mutex_lock(&mp);
+		pause();
+		pthread_mutex_unlock(&mp);
+		return arg;
+	}
+
+	void* spinlock_locker(void *arg)
+	{
+		pthread_spin_lock(&msp);
+		pause();
+		pthread_spin_unlock(&msp);
+		return arg;
+	}
+
+	void* wr_locker(void *arg)
+	{
+		pthread_rwlock_wrlock(&mrwp);
+		pause();
+		pthread_rwlock_wrlock(&mrwp);
+		return arg;
+	}
+
+	void* rd_locker(void *arg)
+	{
+		pthread_rwlock_rdlock(&mrwp);
+		pause();
+		pthread_rwlock_rdlock(&mrwp);
+		return arg;
+	}
+
+	void testLocks()
+	{
+		pthread_mutex_init(&mp, NULL);
+		pthread_spin_init(&msp, PTHREAD_PROCESS_PRIVATE);
+		pthread_rwlock_init(&mrwp, NULL);
+
+		int val = 0;
+
+		// mutex task
+		pthread_t thread1;
+		pthread_create(&thread1, NULL, mutex_locker, &val);
+
+		sleep (1);
+
+		// this thread will be blocked
+		pthread_t thread2;
+		pthread_create(&thread2, NULL, mutex_locker, &val);
+
+		// spinlock task
+		pthread_t thread3;
+		pthread_create(&thread3, NULL, spinlock_locker, &val);
+
+		sleep (1);
+
+		// this thread will be blocked
+		pthread_t thread4;
+		pthread_create(&thread4, NULL, spinlock_locker, &val);
+
+		// read-write lock task
+		pthread_t thread5;
+		pthread_create(&thread5, NULL, wr_locker, &val);
+
+		sleep (1);
+
+		// this thread will wait write lock
+		pthread_t thread6;
+		pthread_create(&thread6, NULL, wr_locker, &val);
+
+		// this thread will wait read lock
+		pthread_t thread7;
+		pthread_create(&thread7, NULL, rd_locker, &val);
+
+		pause();
+
+		pthread_join(thread1, NULL);
+		pthread_join(thread2, NULL);
+		pthread_join(thread3, NULL);
+		pthread_join(thread4, NULL);
+		pthread_join(thread5, NULL);
+		pthread_join(thread6, NULL);
+		pthread_join(thread7, NULL);
+
+		pthread_mutex_destroy(&mp);
+		pthread_spin_destroy(&msp);
+		pthread_rwlock_destroy(&mrwp);
+	}
 }
 
-void* spinlock_locker(void *arg)
+namespace CondVarsAndBarriers
 {
-	pthread_spin_lock(&msp);
-	pause();
-	pthread_spin_unlock(&msp);
-	return arg;
-}
+	pthread_mutex_t mp;
+	pthread_cond_t cond;
+	pthread_barrier_t barrier;
 
-void* wr_locker(void *arg)
-{
-	pthread_rwlock_wrlock(&mrwp);
-	pause();
-	pthread_rwlock_wrlock(&mrwp);
-	return arg;
-}
+	void *condVarWaiter(void *t)
+	{
+	   pthread_mutex_lock(&mp);
+	   while (true)
+	   {
+			pthread_cond_wait(&cond, &mp);
+	   }
+	   pthread_mutex_unlock(&mp);
+	   return t;
+	}
 
-void* rd_locker(void *arg)
-{
-	pthread_rwlock_rdlock(&mrwp);
-	pause();
-	pthread_rwlock_rdlock(&mrwp);
-	return arg;
+	void *barrierWaiter(void *t)
+	{
+	   pthread_barrier_wait(&barrier);
+	   return t;
+	}
+
+	void testCondVarsAndBarriers()
+	{
+		pthread_mutex_init(&mp, NULL);
+		pthread_cond_init(&cond, NULL);
+		pthread_barrier_init(&barrier, NULL, 1);
+
+		int val = 0;
+
+		// cond var task
+		pthread_t thread1;
+		pthread_create(&thread1, NULL, condVarWaiter, &val);
+
+		// barrier task
+		pthread_t thread2;
+		pthread_create(&thread2, NULL, barrierWaiter, &val);
+
+		pause();
+
+		pthread_join(thread1, NULL);
+		pthread_join(thread2, NULL);
+
+		pthread_mutex_destroy(&mp);
+		pthread_cond_destroy(&cond);
+		pthread_barrier_destroy(&barrier);
+	}
 }
 
 int main()
@@ -50,55 +160,8 @@ int main()
 		os.flush();
 	}
 
-	pthread_mutex_init(&mp, NULL);
-	pthread_spin_init(&msp, PTHREAD_PROCESS_PRIVATE);
-	pthread_rwlock_init(&mrwp, NULL);
-
-	int val = 0;
-
-	// mutex task
-	pthread_t thread1;
-	pthread_create(&thread1, NULL, mutex_locker, &val);
-
-	sleep (1);
-
-	// this thread will be blocked
-	pthread_t thread2;
-	pthread_create(&thread2, NULL, mutex_locker, &val);
-
-	// spinlock task
-	pthread_t thread3;
-	pthread_create(&thread3, NULL, spinlock_locker, &val);
-
-	sleep (1);
-
-	// this thread will be blocked
-	pthread_t thread4;
-	pthread_create(&thread4, NULL, spinlock_locker, &val);
-
-	// read-write lock task
-	pthread_t thread5;
-	pthread_create(&thread5, NULL, wr_locker, &val);
-
-	sleep (1);
-
-	// this thread will wait write lock
-	pthread_t thread6;
-	pthread_create(&thread6, NULL, wr_locker, &val);
-
-	// this thread will wait read lock
-	pthread_t thread7;
-	pthread_create(&thread7, NULL, rd_locker, &val);
-
-	pause();
-
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-	pthread_join(thread3, NULL);
-	pthread_join(thread4, NULL);
-	pthread_join(thread5, NULL);
-	pthread_join(thread6, NULL);
-	pthread_join(thread7, NULL);
+	Locks::testLocks();
+	CondVarsAndBarriers::testCondVarsAndBarriers();
 
 	return 0;
 }
